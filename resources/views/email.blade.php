9e568442dd4ecb5005d443aa235377756ee48688
Jeevan sai ravi <ravijeevansai@Ravi-Jeevan-Sai.local>
2020-10-22 21:17:39 +0530

    updated email blade

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,14 +1,10 @@
-Hola <strong>{{ $to_name }}</strong>,
-<br>
-este mensaje es para recodarte que el contrato del cliente Nº <strong>{{ $contract_id }}</strong> vence el <strong>{{ $expiry_date }}</strong>.
-<br>
-Detalles del contrato:
-<strong>{{ $details }}</strong>
-<br>
-Por favor, contacta con el cliente para gestionar su renovación.
-<br>
-Gracias
-<br>
-Saludos
-<br>
-Dirección Comercial
+<div>Hola <strong>{{ $to_name }}</strong>,</div>
+
+<div style="margin-top: 20px">este mensaje es para recodarte que el contrato del cliente Nº <strong>{{ $contract_id }}</strong> vence el <strong>{{ $expiry_date }}</strong>.</div>
+<div style="margin-top: 5px">Detalles del contrato:</div>
+<div><strong>{{ $details }}</strong></div>
+<div style="margin-top: 5px">Por favor, contacta con el cliente para gestionar su renovación.</div>
+
+<div style="margin-top: 20px">Gracias</div>
+<div>Saludos</div>
+<div>Dirección Comercial</div>
